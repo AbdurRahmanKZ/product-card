a67681fd892337579fc186a9eb4f148b63b45c1e
AbdurRahmanKZ <titantito2002@gmail.com>
2026-02-04 08:52:57 +0500

    comment scripts

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// Объявление переменных и присвоение html элементов
 const mainTitle = document.querySelector('.main-title');
 const colorRedTitleButton = document.getElementById('color-red-title-button');
 
+// Создание слушателя событий для заголовка с выводом его в консоль
 mainTitle.addEventListener('mouseover', () => {
   console.log(mainTitle);
 });
 
+// Создание слушателя событий для кнопки с покраской заголовка в красный с classList.toggle
 colorRedTitleButton.addEventListener('click', () => {
   mainTitle.classList.toggle('red');
 });
