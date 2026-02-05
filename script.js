7fa14acdd9649583bfa39e934e2d087ca23712c6
AbdurRahmanKZ <titantito2002@gmail.com>
2026-02-05 14:25:23 +0500

    fix naming

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Объявление переменных и присвоение html элементов
 const mainTitle = document.querySelector('.main-title');
-const buttonColorToggleButton = document.getElementById('button-color-toggle-button');
+const toggleColorButton = document.getElementById('toggle-color-button');
 // Создание слушателя событий для заголовка с выводом его в консоль
 mainTitle.addEventListener('mouseover', () => {
   console.log(mainTitle.textContent);
 });
 // Создание слушателя событий для кнопки с покраской заголовка в красный с classList.toggle
-buttonColorToggleButton.addEventListener('click', () => {
-  buttonColorToggleButton.classList.toggle('red');
+toggleColorButton.addEventListener('click', () => {
+  toggleColorButton.classList.toggle('red');
 });
