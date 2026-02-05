7021fc3dd851f20182ca0e81581058e1fb323381
AbdurRahmanKZ <titantito2002@gmail.com>
2026-02-05 07:44:11 +0500

    fix renaming and delete lines

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 // Объявление переменных и присвоение html элементов
 const mainTitle = document.querySelector('.main-title');
-const buttonColorToggleBtn = document.getElementById('button-color-toggle-button');
-
+const buttonColorToggleButton = document.getElementById('button-color-toggle-button');
 // Создание слушателя событий для заголовка с выводом его в консоль
 mainTitle.addEventListener('mouseover', () => {
   console.log(mainTitle.textContent);
 });
-
 // Создание слушателя событий для кнопки с покраской заголовка в красный с classList.toggle
-buttonColorToggleBtn.addEventListener('click', () => {
-  buttonColorToggleBtn.classList.toggle('red');
+buttonColorToggleButton.addEventListener('click', () => {
+  buttonColorToggleButton.classList.toggle('red');
 });
