c59bf4adb9eeda917f721af40ab5e6f66c985fb7
AbdurRahmanKZ <titantito2002@gmail.com>
2026-04-23 15:36:23 +0600

    modal window work

--- homework-9.js
+++ homework-9.js
@@ -1,6 +1,11 @@
 const formEmail = document.querySelector(".footer-top-center-bar-form");
 const openRegisterBtn = document.querySelector(".register-button");
 const closeRegisterBtn = document.querySelector(".close-register-button");
+const overlay = document.querySelector(".overlay")
+const modal = document.querySelector(".modal");
+const formModal = document.querySelector(".modal-form");
+const modalInput = document.querySelector(".modal-input");
+const birthDateInput = document.getElementById("form-birthdate");
 
 formEmail.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -11,22 +16,22 @@ formEmail.addEventListener("submit", (e) => {
   const data = Object.fromEntries(formData.entries());
   console.log(data)
 });
-
-
-
-
-
-
-
-
-
-
-
-openRegisterButton.addEventListener("click", () => {
+closeRegisterBtn.addEventListener("click", () => {
+  modal.classList.remove("modal-showed");
+  overlay.classList.remove("overlay-showed");
+})
+openRegisterBtn.addEventListener("click", () => {
   modal.classList.add("modal-showed");
   overlay.classList.add("overlay-showed");
 });
-closeRegisterButton.addEventListener("click", () => {
+formModal.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!formModal.checkValidity()) {
+    return;
+  }
+  const formData = new FormData(formModal);
+  const obj = Object.fromEntries(formData.entries());
+  console.log(obj);
   modal.classList.remove("modal-showed");
   overlay.classList.remove("overlay-showed");
 });
