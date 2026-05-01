9ee4b4fc50b6dab8f8e078bb9f35fa8eda4367a7
AbdurRahmanKZ <titantito2002@gmail.com>
2026-05-01 16:44:33 +0500

    add bit template

--- async/async.js
+++ async/async.js
@@ -1,47 +1,39 @@
-const containerUser = document.querySelector(".catalog");
+const containerUser = document.querySelector(".users-catalog");
 const userTemplate = document.querySelector("#users-catalog");
+const status = document.querySelector(".status");
 
 function renderUsers(users) {
   containerUser.innerHTML = "";
   users.forEach(user => {
     const userClone = userTemplate.content.cloneNode(true);
-    userClone.
+    userClone.querySelector(".user-id").textContent = user.id
+    userClone.querySelector(".user-name").textContent = user.name
+    userClone.querySelector(".user-surname").textContent = user.surname
+    userClone.querySelector(".user-email").textContent = user.email
+    userClone.querySelector(".user-age").textContent = user.age
+    containerUser.appendChild(userClone);
   });
 } 
-
-
-
-
-
-
-
-
-
-
-
-
-
-const status = document.querySelector(".status");
 
 async function loadUsers() {
   const savedUsers = localStorage.getItem("users");
   if (savedUsers) {
-    const users = JSON.parse(savedUsers);
+    const users1 = JSON.parse(savedUsers);
 
     setTimeout(() => {
-      status.textContent = "Данные получены из localStorage";
-      console.log("взяли из localStorage", users);
+      status.textContent = "";
+      renderUsers(users1);
     }, 3000);
   } else {
     const response = await fetch("./users.json");
-    const users = await response.json();
+    const users1 = await response.json();
     
     setTimeout(() => {
-      localStorage.setItem("users", JSON.stringify(users));
-      status.textContent = "Данные загружены в localStorage";
-      console.log(users);
+      localStorage.setItem("users", JSON.stringify(users1));
+      status.textContent = "";
+      renderUsers(users1);
     }, 3000);
   }
 }
 
-loadUsers();
+loadUsers()
