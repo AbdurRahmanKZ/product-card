c63ae36a87e2be910e1cd90fccc5ef1c20177e19
AbdurRahmanKZ <titantito2002@gmail.com>
2026-05-02 11:01:52 +0500

    add fix

--- async/async.js
+++ async/async.js
@@ -8,6 +8,10 @@ const getAllCardsBtn = document.querySelector("#get-all-button");
 
 function renderUsers(users) {
   containerUser.innerHTML = "";
+  if (!Array.isArray(users) || users.length === 0) {
+    status.textContent = "нет данных";
+    return;
+  }
   users.forEach(user => {
     const userClone = userTemplate.content.cloneNode(true);
     userClone.querySelector(".user-id").textContent = "index:" + user.id
@@ -17,37 +21,57 @@ function renderUsers(users) {
     userClone.querySelector(".user-age").textContent = "age: " + user.age + " years"
     containerUser.appendChild(userClone);
   });
-} 
+}
 
-async function loadUsers() {
-  const savedUsers = localStorage.getItem("users");
-  if (savedUsers) {
-    const users1 = JSON.parse(savedUsers);
+let users
 
-    setTimeout(() => {
-      status.textContent = "";
-      renderUsers(users1);
-    }, 3000);
-  } else {
-    const response = await fetch("./users.json");
-    const users1 = await response.json();
-    
-    setTimeout(() => {
-      localStorage.setItem("users", JSON.stringify(users1));
-      status.textContent = "";
-      renderUsers(users1);
-    }, 3000);
-  }
+async function loadAllCardsToLocal() {
+  const response = await fetch("./users.json");
+  const users = await response.json();
+  localStorage.setItem("users", JSON.stringify(users));
+}
+
+function loadAllCardsToVar() {
+  const savedUsers = JSON.parse(localStorage.getItem("users"));
+  users = savedUsers;
+}
+
+async function loadAllCards() {
+  status.textContent = "Данные загружаются";
+  setTimeout(async () => {
+    await loadAllCardsToLocal();
+    loadAllCardsToVar();
+    renderUsers(users);
+    status.textContent = ""
+  }, 3000);
+}
+async function init() {
+  setTimeout(async () => {
+    await loadAllCardsToVar();
+    renderUsers(users);
+  }, 3000);
 }
 
 deleteAllCardsBtn.addEventListener("click", () => {
-  //
-});
-deleteChosenCardBtn.addEventListener("click", () => {
-  //
-});
-getAllCardsBtn.addEventListener("click", () => {
-  //
+  localStorage.setItem("users", JSON.stringify([]));
+  users = [];
+  renderUsers(users);
 });
 
-loadUsers()
+deleteChosenCardBtn.addEventListener("click", () => {
+  const inputCard = Number(deleteChosenCardInput.value);
+  users = users.filter((user) => {
+    return user.id !== inputCard;
+  })
+  localStorage.setItem("users", JSON.stringify(users));
+  status.textContent ="удаляем"
+  setTimeout(() => {
+    renderUsers(users);
+  }, 1000);
+});
+
+getAllCardsBtn.addEventListener("click", () => {
+  loadAllCards();
+});
+
+init();
