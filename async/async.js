b8895a61d951d8e6bfe3f78dcec59078edb70987
AbdurRahmanKZ <titantito2002@gmail.com>
2026-05-03 13:59:45 +0500

    feat architecture, and logics

--- async/async.js
+++ async/async.js
@@ -6,65 +6,77 @@ const deleteChosenCardBtn = document.querySelector("#delete-chosen-button");
 const deleteChosenCardInput = document.querySelector("#delete-chosen-input");
 const getAllCardsBtn = document.querySelector("#get-all-button");
 
-let users
+let users;
+let userFromFile;
 
-function renderUsers(users) {
-  containerUser.innerHTML = "";
-  if (!Array.isArray(users) || users.length === 0) {
-    status.textContent = "нет данных";
-    return;
-  }
-  users.forEach(user => {
-    const userClone = userTemplate.content.cloneNode(true);
-    userClone.querySelector(".user-id").textContent = "index:" + user.id
-    userClone.querySelector(".user-name").textContent = "name: " + user.name
-    userClone.querySelector(".user-surname").textContent = "surname: " + user.surname
-    userClone.querySelector(".user-email").textContent = "email: " + user.email
-    userClone.querySelector(".user-age").textContent = "age: " + user.age + " years"
-    containerUser.appendChild(userClone);
-  });
-  status.textContent = ""
-}
-
-function loadAllCardsToVar() {
-  const savedUsers = JSON.parse(localStorage.getItem("users"));
-  users = savedUsers;
-}
-
-async function ifFirstLoad() {
-  if (users === null) {
-    await loadAllCardsToLocal();
-  }
-}
-
-async function init() {
-  setTimeout(async() => {
-    await ifFirstLoad();
-    loadAllCardsToVar();
-    renderUsers(users);
-  }, 1000);
-}
-
-async function loadAllCardsToLocal() {
-  const response = await fetch("./users.json");
-  const users = await response.json();
+function setLocal(users) {
   localStorage.setItem("users", JSON.stringify(users));
 }
 
-async function loadAllCards() {
-  status.textContent = "Данные загружаются";
-  setTimeout(async () => {
-    await loadAllCardsToLocal();
-    loadAllCardsToVar();
-    renderUsers(users);
+function loadLocal() {
+  const savedUsers = JSON.parse(localStorage.getItem("users"));
+  return savedUsers;
+}
+
+async function loadFentch() {
+  try {
+    const response = await fetch("./users.json");
+    if (!response.ok) {
+      throw new Error("Ошибка загрузки файла")
+    }
+    const usersFromFile = await response.json();
+    if (!Array.isArray(usersFromFile)) {
+      throw new Error("Данные не являются массивом");
+    }
+    if (usersFromFile.length === 0) {
+      throw new Error("Файл пустой");
+    }
+    return usersFromFile;
+  } catch (err) {
+    status.textContent = err.message;
+  }
+}
+
+
+function renderUsers(users) {
+  containerUser.innerHTML = "";
+  setTimeout(() => {
+    if (!Array.isArray(users) || users.length === 0) {
+      status.textContent = "нет данных"
+      setTimeout(() => {
+        status.textContent ="";
+      }, 1000);
+      return;
+    }
+    users.forEach(user => {
+      const userClone = userTemplate.content.cloneNode(true);
+      userClone.querySelector(".user-id").textContent = "index:" + user.id
+      userClone.querySelector(".user-name").textContent = "name: " + user.name
+      userClone.querySelector(".user-surname").textContent = "surname: " + user.surname
+      userClone.querySelector(".user-email").textContent = "email: " + user.email
+      userClone.querySelector(".user-age").textContent = "age: " + user.age + " years"
+      containerUser.appendChild(userClone);
+    });
     status.textContent = ""
   }, 1000);
 }
 
+async function onLoad() {
+  const savedUsers = loadLocal();
+  if (Array.isArray(savedUsers)) {
+    users = savedUsers;
+    renderUsers(users);
+    return;
+  }
+  users = await loadFentch();
+  setLocal(users);
+  renderUsers(users);
+}
+
 deleteAllCardsBtn.addEventListener("click", () => {
-  localStorage.setItem("users", JSON.stringify([]));
   users = [];
-  status.textContent ="удаляем"
+  localStorage.setItem("users", JSON.stringify(users));
+  status.textContent = "удаляем"
   setTimeout(() => {
     renderUsers(users);
   }, 1000);
@@ -76,14 +88,28 @@ deleteChosenCardBtn.addEventListener("click", () => {
     return user.id !== inputCard;
   })
   localStorage.setItem("users", JSON.stringify(users));
-  status.textContent ="удаляем"
+  status.textContent = "удаляем"
   setTimeout(() => {
     renderUsers(users);
   }, 1000);
 });
 
-getAllCardsBtn.addEventListener("click", () => {
-  loadAllCards();
+getAllCardsBtn.addEventListener("click", async () => {
+  status.textContent = "Данные загружаются";
+  userFromFile = await loadFentch();
+  setTimeout(() => {
+    if (JSON.stringify(userFromFile) === JSON.stringify(users)) {
+      status.textContent = "уже загружено"
+      setTimeout(() => {
+        status.textContent = ""
+      }, 1000);
+      return;
+    } else {
+      users = userFromFile;
+      localStorage.setItem("users", JSON.stringify(users));
+      renderUsers(users);
+    }
+  }, 1000);
 });
 
-init();
+onLoad();
